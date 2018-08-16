d4992fce0120403950ec164bf32245c43c2e35f4
codezilla <garubamalik@gmail.com>
2018-08-16 10:00:04 +0000

    another commit

--- boards/tests.py
+++ boards/tests.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 from django.urls import reverse, resolve
 from .views import home, board_topics, new_topic
-from .models import Board
+from .models import Board, Topic, Post
 # Create your tests here.
 
 class HomeTest(TestCase):
@@ -45,10 +46,18 @@ class BoardTopicsTest(TestCase):
         homepage_url = reverse('home')
         self.assertContains(response, 'href="{0}"'.format(homepage_url))
 
+    def test_board_topic_view_contain_link_newTopic(self):
+        board_topic_url = reverse('board_topics', kwargs={'pk':1})
+        response = self.client.get(board_topic_url)
+        new_topic_url = reverse('home')
+        self.assertContains(response, 'href="{0}"'.format(new_topic_url))
+
+
 
 class NewTopicTest(TestCase):
     def setUp(self):
         self.board = Board.objects.create(name='Flask', description='Flask Discussion')
+        User.objects.create_user(username='tester', email='tester@test.com', password='123')
         self.url = reverse('new_topic', kwargs={'pk':self.board.id})
         self.response = self.client.get(self.url)
 
@@ -63,3 +72,34 @@ class NewTopicTest(TestCase):
     def test_new_topic_confirm_right_view(self):
         view = resolve('/1/new')
         self.assertEquals(view.func, new_topic)
+
+    def test_new_topic_view_contain_link_back_board_topic(self):
+        new_topic = reverse('new_topic', kwargs={'pk': self.board.id})
+        response = self.client.get(new_topic)
+        board_topic = reverse('board_topics', kwargs={'pk':self.board.id })
+        self.assertContains(response, 'href="{0}"'.format(board_topic))
+
+    def test_csrf(self):
+        new_topic_url = reverse('new_topic',kwargs={'pk': 1})
+        response = self.client.get(new_topic_url)
+        self.assertContains(response, 'csrfmiddlewaretoken')
+
+    def test_new_topic_valid_post(self):
+        new_topic_url = reverse('new_topic', kwargs={'pk':1})
+        data = {'subject':'test', 'message':'just testing',}
+        response = self.client.post(new_topic_url, data)
+        self.assertTrue(Topic.objects.exists())
+        self.assertTrue(Post.objects.exists())
+
+    def test_invalid_post_data(self):
+        url = reverse('new_topic', kwargs={'pk':1})
+        response = self.client.post(url, {})
+        self.assertEquals(response.status_code, 200)
+
+    def test_invalid_empty_post(self):
+        url=reverse('new_topic', kwargs={'pk':1})
+        data = {'subject':'', 'message':''}
+        response= self.client.post(url, data)
+        self.assertEquals(response.status_code, 200)
+        self.assertFalse(Topic.objects.exists())
+        self.assertFalse(Post.objects.exists())
