468c11ab9016cebe4e426fd72667cf08434631d4
codezilla <garubamalik@gmail.com>
2018-08-26 15:27:38 +0000

    added new

--- myproject/urls.py
+++ myproject/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('boards.urls')),
     path('account/', include('accounts.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
+    #path('accounts/', include('django.contrib.auth.urls')),
 ]
